0a125c9eb8930972780862a1e880f5817723890d
Abhishek nayak <sahilservice200@gmail.com>
2020-09-14 14:35:56 +0530

    Version 2

---
+++ main.js
@@ -0,0 +1,35 @@
+// // Get DOM Elements
+// const modal = document.querySelector('#my-modal');
+// const modalBtn = document.querySelector('#modal-btn');
+// const closeBtn = document.querySelector('.close');
+
+// // Events
+// modalBtn.addEventListener('click', openModal);
+// closeBtn.addEventListener('click', closeModal);
+// window.addEventListener('click', outsideClick);
+
+// // Open
+// function openModal() {
+//   modal.style.display = 'block';
+// }
+
+// // Close
+// function closeModal() {
+//   modal.style.display = 'none';
+// }
+
+// // Close If Outside Click
+// function outsideClick(e) {
+//   if (e.target == modal) {
+//     modal.style.display = 'none';
+//   }
+// }
+
+
+
+// var myModal = document.getElementById('myModal')
+// var myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus()
+// })
